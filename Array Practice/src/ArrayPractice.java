1f2fe8f47fef90ed3f4a56112361c7e74eee6b30
nicholas.giacomozzi <nicholas.giacomozzi@mu-c1-03.mullen.pvt.k12.co.us>
2019-09-05 11:01:38 -0600

    Finished Array Practice

--- Array Practice/src/ArrayPractice.java
+++ Array Practice/src/ArrayPractice.java
@@ -12,10 +12,15 @@ public class ArrayPractice
 				poloColor[3] = "Light Blue";
 				poloColor[4] = "White";
 
+				System.out.println("Your polo color choices are:");
 				for (String p : poloColor)
 					{
-						System.out.println("Your polo color choices are: " + p);
+						System.out.println(p);
 					}
+
+				int randomNumber = (int) (Math.random() * poloColor.length);
+
+				System.out.println("You should wear the color " + poloColor[randomNumber] + " today!");
 
 			}
 
